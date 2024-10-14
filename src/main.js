1a007df4dc82dcec3bfb3d06b69d41a3d5718823
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2024-10-14 17:33:54 -0300

    storybook init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+// import './assets/main.css'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
